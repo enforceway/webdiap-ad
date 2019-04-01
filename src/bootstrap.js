238a9d29622a6d324253e06dcafb1194fa33a279
enforceway <enforceway@163.com>
2019-04-01 18:12:55 +0800

    国际化

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,10 @@
-'use strict';
+(function() {
+  'use strict';
 
 /* Controllers */
 
-angular.module('app')
-  .controller('baseController', ['$scope', '$translate', '$localStorage', '$window', 
+var app = angular.module('app');
+app.controller('baseController', ['$scope', '$translate', '$localStorage', '$window', 
     function(              $scope,   $translate,   $localStorage,   $window ) {
       // add 'ie' classes to html
       var isIE = !!navigator.userAgent.match(/MSIE/i);
@@ -74,3 +75,31 @@ angular.module('app')
       }
 
   }]);
+
+app.config(['$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+    function ($controllerProvider,   $compileProvider,   $filterProvider,   $provide) {
+        
+        // lazy controller, directive and service
+        app.controller = $controllerProvider.register;
+        app.directive  = $compileProvider.directive;
+        app.filter     = $filterProvider.register;
+        app.factory    = $provide.factory;
+        app.service    = $provide.service;
+        app.constant   = $provide.constant;
+        app.value      = $provide.value;
+    }
+  ])
+  .config(['$translateProvider', function($translateProvider){
+    // Register a loader for the static files
+    // So, the module will search missing translation tables under the specified urls.
+    // Those urls are [prefix][langKey][suffix].
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'l10n/',
+      suffix: '.js'
+    });
+    // Tell the module what language to use by default
+    $translateProvider.preferredLanguage('en');
+    // Tell the module to store the language in the local storage
+    $translateProvider.useLocalStorage();
+  }]);
+})();
