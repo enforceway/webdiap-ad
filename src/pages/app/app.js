562c5f25c9d26c5b855bef579349f02ffba35e07
enforceway <enforceway@163.com>
2019-07-18 21:20:00 +0800

    添加菜单高亮

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -3,8 +3,14 @@
 /* Controllers */
 
 angular.module('app')
-  .controller('AppController', ['$scope', '$translate', '$localStorage', '$window', 
-    function($scope,   $translate,   $localStorage,   $window ) {
-      
-  }]);
-  
+.controller('AppController', ['$state', '$scope', '$translate', '$localStorage', '$window', 
+  function($state, $scope, $translate, $localStorage, $window) {
+  $scope.ifActiveMenu = function(target) {
+    var ifActive = false;
+    if(Object.prototype.toString.call(target).toLowerCase() != '[object array]') {
+      target = [target]
+    }
+    ifActive = (target.indexOf($state.current.name) > -1)
+    return ifActive;
+  };
+}]);
