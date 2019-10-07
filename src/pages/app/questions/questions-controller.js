2b9f4b32f3f188870f16cb490dcdc0f789843795
欧阳明择 <enforceway@163.com>
2019-10-07 12:33:11 +0800

    添加分页插件

--- src/pages/app/questions/questions-controller.js
+++ src/pages/app/questions/questions-controller.js
@@ -2,9 +2,14 @@
 
 angular.module('app').controller('QuestionsController', ['$diBootstrapPager', '$diConfirm', '$scope', '$G', '$diModal', '$diResource', '$http', 'toaster', 
 function($diBootstrapPager, $diConfirm, $scope, $G, $diModal, $diResource, $http, toaster) {
-    $scope.questions = [];
     $scope.searchForm = {
-        keyWord: ''
+        keyWord: '',
+        questions: [],
+        pagination: {
+            total: 0,
+            pageSize: 0,
+            pageIndex: 0
+        }
     };
     $scope.goToUpdate = function(item) {
         var $ns = $scope.$new();
@@ -67,10 +72,10 @@ function($diBootstrapPager, $diConfirm, $scope, $G, $diModal, $diResource, $http
             }
         }).then(function(res) {
             $scope.$apply(function () {
-                $scope.questions = res.data;
+                $scope.searchForm.questions = res.data;
                 $diBootstrapPager.bpager($('.callBackPager'), {
                     totalCount: res.pagination.total,
-                    // showCount: 0,
+                    // showCount: res.pagination.curPage,
                     limit: res.pagination.pageSize
                 })
             });
@@ -82,64 +87,5 @@ function($diBootstrapPager, $diConfirm, $scope, $G, $diModal, $diResource, $http
         });
     };
     $scope.search();
-    // $scope.filterOptions = {
-    //     filterText: "",
-    //     useExternalFilter: true
-    // }; 
-    // $scope.totalServerItems = 0;
-    $scope.pagingOptions = {
-        pageSizes: [250, 500, 1000],
-        pageSize: 250,
-        currentPage: 1
-    };
     $scope.myData = [];
-    // $scope.setPagingData = function(data, page, pageSize) {
-    //     var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-    //     // debugger
-    //     $scope.myData = pagedData;
-    //     $scope.totalServerItems = data.length;
-    //     if (!$scope.$$phase) {
-    //         $scope.$apply();
-    //     }
-    // };
-    // $scope.getPagedDataAsync = function (pageSize, page, searchText) {
-    //     setTimeout(function () {
-    //         var data;
-    //         if (searchText) {
-    //             var ft = searchText.toLowerCase();
-    //             $http.get('mock/largeload.json').success(function (largeLoad) {    
-    //                 data = largeLoad.filter(function(item) {
-    //                     return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
-    //                 });
-    //                 $scope.setPagingData(data,page,pageSize);
-    //             });            
-    //         } else {
-    //             $http.get('mock/largeLoad.json').success(function (largeLoad) {
-    //                 $scope.setPagingData(largeLoad,page,pageSize);
-    //             });
-    //         }
-    //     }, 100);
-    // };
-
-    // $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-
-    // $scope.$watch('pagingOptions', function (newVal, oldVal) {
-    //     if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
-    //       $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    //     }
-    // }, true);
-    // $scope.$watch('filterOptions', function (newVal, oldVal) {
-    //     if (newVal !== oldVal) {
-    //       $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    //     }
-    // }, true);
-
-    // $scope.gridOptions = {
-    //     data: 'myData',
-    //     enablePaging: true,
-    //     showFooter: true,
-    //     totalServerItems: 'totalServerItems',
-    //     pagingOptions: $scope.pagingOptions,
-    //     filterOptions: $scope.filterOptions
-    // };
 }]);
